4a2266c61c39cf4dfec027f55fdbd42f70942621
Michele Ursino <mursino@linkedin.com>
2021-02-22 16:44:20 -0800

    adding a test

--- src/calc_test.py
+++ src/calc_test.py
@@ -11,6 +11,13 @@ class TestStringMethods(unittest.TestCase):
         # Make sure 4 - 3 = 1
         self.assertEqual(sub(4, 3), 1, 'subtracting three from four')
 
+    def test_sub_2arg(self):
+        # Make sure 4 - 3 = 1
+        self.assertEqual(sub(4, 3), 1, 'subtracting three from four')
+
+    def test_sub_3arg_0(self)
+        self.assertEqual(sub(0,0,0),0, 'zeros are ok')
+
 
 if __name__ == '__main__':
     unittest.main()
